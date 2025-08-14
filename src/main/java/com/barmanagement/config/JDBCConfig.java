1df3acc0d3d16da8f3d42cca85d7e96a178d902a
nghiapro537 <donghia.cc>
2025-08-14 18:10:58 +0700

    nghia fix lại LoginController

--- src/main/java/com/barmanagement/config/JDBCConfig.java
+++ src/main/java/com/barmanagement/config/JDBCConfig.java
@@ -5,7 +5,7 @@ public interface JDBCConfig {
     String PORT = "3306";
     String DBNAME = "bar_management";
     String USERNAME = "root";      // đổi nếu khác
-    String PASSWORD = "2004";    // đổi nếu khác
+    String PASSWORD = "Nghia537991014@";    // đổi nếu khác
 
     String CONNECTION_URL = "jdbc:mysql://" + HOSTNAME + ":" + PORT + "/" + DBNAME
             + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
